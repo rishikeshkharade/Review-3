c546fe91d1172d43cc98a0d25b66dea2843beb48
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-22 14:23:34 +0530

    Password Strength Checker

---
+++ PasswordStrengthChecker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Review_3
+{
+    internal class PasswordStrengthChecker
+    {
+        public static void MainMethod()
+        {
+            Console.WriteLine("Enter a password to check its strength: ");
+            string password = Console.ReadLine();
+
+            if (IsStrongPassword(password))
+            {
+                Console.WriteLine("The password is strong");
+            }
+            else
+            {
+                Console.WriteLine("The password is weak");
+            }
+
+
+        }
+
+        static bool IsStrongPassword(string password)
+        {
+            Regex regex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$");
+
+            return regex.IsMatch(password);
+        }
+    }
+}

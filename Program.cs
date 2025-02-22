a85cda47f10d00e0b6c89012b5bea026b209a9a5
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-22 14:14:50 +0530

    Main of All Files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Review_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //EmailValidation.EmailValidationMain();
+        //PasswordStrengthChecker.MainMethod();
+        //PhoneNumberValidator.PhoneNumber();
+        //FindDuplicateWords.DuplicateWords();
+        //BasicDivisionProgram.DivisionProgram();
+        //CustomAgeValidator.AgeValidator();
+        //BankingSystem.BankingSystemMain();
+        //ProductAvailability.Availability();
+        //Even_OddChecker.EvenOdd();
+        //StringLengthComparison.StringLength();
+        //NumberTransformation.NumberTransformationMain();
+        FindMaximum.Maximum();
+
+    }
+}

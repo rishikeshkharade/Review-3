94e070cddf11f09549dba5ab3e212f87b876001e
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-22 14:22:16 +0530

    Find the maximum number

---
+++ Maximum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review_3
+{
+
+    class FindMaximum
+    {
+        public static void Maximum()
+        {
+
+            Func<int, int, int> max = (a, b) => a > b ? a : b;
+
+            Console.WriteLine("Enter the first number:");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int maximum = max(firstNumber, secondNumber);
+
+            Console.WriteLine($"The maximum of {firstNumber} and {secondNumber} is: {maximum}");
+        }
+    }
+}

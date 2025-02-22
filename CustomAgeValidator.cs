a00bd0adb79b76b70206ef4f0bb3fcdbb6c733e1
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-22 14:19:45 +0530

    Custom Age Validator

---
+++ CustomAgeValidator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review_3
+{
+    internal class CustomAgeValidator
+    {
+        public static void AgeValidator()
+        {
+            try
+            {
+                Console.WriteLine("Enter your age:");
+                string input = Console.ReadLine();
+                int age = ValidateAge(input);
+                Console.WriteLine("Your age is " + age);
+            }
+            catch (AgeOutOfRangeException ae)
+            {
+                Console.WriteLine(ae.Message);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static int ValidateAge(string input)
+        {
+            if (!int.TryParse(input, out int age))
+                throw new FormatException("Input is not a valid integer.");
+            
+
+            if (age < 0 || age > 150)
+                throw new AgeOutOfRangeException("Age must be between 0 and 150");
+    
+        return age;
+        }
+    }
+
+    public class AgeOutOfRangeException : Exception
+    {
+        public AgeOutOfRangeException(string message) : base(message) { }
+    }
+}

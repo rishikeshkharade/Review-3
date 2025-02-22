b5b1b3565824f0b030b76a8cbdf70a133acb0cb2
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-22 14:19:12 +0530

    Palindrome Checker

---
+++ PalindromeChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review_3
+{
+    public class PalindromeChecker
+    {
+        public bool IsPalindrome(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+
+            string normalizedInput = input.Replace(" ", "").ToLower();
+
+            char[] charArray = normalizedInput.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedInput = new string(charArray);
+
+            return normalizedInput == reversedInput;
+        }
+    }
+}
---
+++ PalindromeCheckerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Review_3;
+namespace Tests
+{
+    public class PalindromeCheckerTests
+    {
+        private PalindromeChecker palindromeCheck;
+
+        [SetUp]
+        public void Setup()
+        {
+            palindromeCheck = new PalindromeChecker();
+        }
+
+        [Test]
+        public void IsPalindrome()
+        {
+
+            string input = "madam";
+
+
+            bool result = palindromeCheck.IsPalindrome(input);
+
+
+            Assert.IsTrue(result);
+        }
+
+
+        [Test]
+        public void IsNotPalindrome()
+        {
+
+            string input = "hello";
+
+
+            bool result = palindromeCheck.IsPalindrome(input);
+
+
+            Assert.IsFalse(result);
+        }
+    }
+}

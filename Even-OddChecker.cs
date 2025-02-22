493829ac2025d1aaac58ac59091c19148e1db9a7
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-22 14:21:05 +0530

    Even Odd Checker with Lambda Expression

---
+++ Even-OddChecker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review_3
+{
+    internal class Even_OddChecker
+    {
+        public static void EvenOdd()
+        {
+            Console.WriteLine("Enter a number:");
+            string input = Console.ReadLine();
+
+            bool isEven(int number) => number % 2 == 0;
+            bool isOdd(int number) => number % 2 != 0;
+
+
+            try
+            {
+                int number = int.Parse(input);
+                if (isEven(number))
+                    Console.WriteLine("The number is even");
+                else Console.WriteLine("The number is odd");
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Invalid input");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
